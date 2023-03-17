7ea65755cf8b873e9337e6539338bd22da4fb87f
Nahu <montinahuel@gmail.com>
2023-03-17 14:31:45 +0700

    Mejoras Navbar

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 window.onscroll = function() {scrollFunction()};
 window.onclick = function(get_id) {};
+document.querySelector(".navbar-toggler").addEventListener("click", animateBars);
+
+var line1__bars = document.querySelector(".line1__bars_menu");
+var line2__bars = document.querySelector(".line2__bars_menu");
+var line3__bars = document.querySelector(".line3__bars_menu");
 
 function scrollFunction() {
     if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-        document.getElementById("navbar").style.backgroundColor = "rgb(18, 0, 117)";
+        document.getElementById("navbar").style.backgroundColor = "rgb(18, 0, 117,.8)";
     } else {
         document.getElementById("navbar").style.backgroundColor = "";
     }
@@ -25,4 +30,10 @@ function clickBotonNav(get_id) {
         document.getElementById("hola").classList.remove("Active");
         document.getElementById("adios").classList.add("Active");
     }
+}
+
+function animateBars() {
+    line1__bars.classList.toggle("activeline1__bars_menu");
+    line2__bars.classList.toggle("activeline2__bars_menu");
+    line3__bars.classList.toggle("activeline3__bars_menu");
 }
